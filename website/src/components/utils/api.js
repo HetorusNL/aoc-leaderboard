903c7a547f75fcf5ab11949087a8b871b4b7e1d1
HetorusNL <tim@hetorus.nl>
2024-07-26 00:03:38 +0200

    added withCredentials to axios

--- website/src/components/utils/api.js
+++ website/src/components/utils/api.js
@@ -3,6 +3,8 @@ import axios from "axios";
 const API_ENDPOINT = "https://api.aoc.hetorus.nl";
 // const API_ENDPOINT = "http://127.0.0.1:5000"; // for testing purposes
 
+axios.defaults.withCredentials = true;
+
 export const apiLeaderboard = async (edition, leaderboard) => {
   return await axios.get(`${API_ENDPOINT}/${edition}/${leaderboard}`);
 };
